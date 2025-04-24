0351a8de8828a6e9f56037bea42f2ffbe20db05a
fasegree <fasegree@gmail.com>
2025-04-24 08:44:18 +0200

    clean-code sidebar

--- src/components/ui/sidebar/SidebarTrigger.vue
+++ src/components/ui/sidebar/SidebarTrigger.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { PanelLeft } from 'lucide-vue-next'
-import { useSidebar } from './utils'
-import { Bars3Icon } from '@heroicons/vue/16/solid'
+import { useSidebar } from "./utils";
+import { Bars3Icon } from "@heroicons/vue/16/solid";
 
-const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
-
-const { toggleSidebar } = useSidebar()
+const { toggleSidebar } = useSidebar();
 </script>
 
 <template>
-  
-<Bars3Icon 
-data-sidebar="trigger"
-  class="size-7 cursor-pointer"
-  @click="toggleSidebar"/>
-
+  <Bars3Icon
+    data-sidebar="trigger"
+    class="size-7 cursor-pointer"
+    @click="toggleSidebar"
+  />
 </template>
